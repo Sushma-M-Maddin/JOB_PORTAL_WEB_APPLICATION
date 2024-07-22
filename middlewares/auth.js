697cc455659aaeaac5d385ae93c9bfb620f39b6e
Sushma-M-Maddin <sushmamaddin@gmail.com>
2024-07-22 14:09:14 +0530

    Updated auth.js file

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,3 @@
-// import { catchAsyncError } from "./catchAsyncError";
-// import ErrorHandler from "./error.js";
-// import jwt from "jsonwebtoken";
-// import { User } from "../models/userSchema.js";
-
-// export const isAuthorized = catchAsyncError(async (req, res, next) => {
-//   const { token } = req.cookies;
-//   if (!token) {
-//     return next(new ErrorHandler("User not authorized", 400));
-//   }
-//   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-//   req.user = await User.findById(decoded.id);
-//   next();
-// });
 import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/userSchema.js";
